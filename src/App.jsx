187cf0074783a3ca07f6eeee9826268979c6d874
vallarasuk <vallarasu.kanthasamy@thidiff.com>
2024-10-12 19:58:09 +0530

    deploy file changes updated commit

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/ChemicalProject" element={<Home />} />
         <Route path="/about-us" element={<AboutUs />} />
         <Route path="/services" element={<Services />} />
         <Route path="/gallery" element={<Gallery />} />
